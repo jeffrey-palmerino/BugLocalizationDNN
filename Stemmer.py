e879a369c6ffd876bf14cd43b22f256a09d873f5
Andy DiStasi <add5980@rit.edu>
2018-04-20 00:18:30 -0400

    Added ability to parse source code for comments to Stemmer.py

--- Stemmer.py
+++ Stemmer.py
@@ -1,10 +1,26 @@
 from nltk.stem.porter import *
 import re
 import pprint
+import string
 
+comments = []
+'''
+Parses test and gets comments between /** and */, stripping out asterisks and tabs
+'''
+def getComments(text):
+    for comment in re.findall(r'\*\*(.*?)\*\/', text, re.S):
+        cleaned = re.sub('\n+', ',', re.sub(r'[ *]+', ' ', comment.strip()).strip())
+        comments.append(re.sub(' +', ' ', cleaned.translate(string.maketrans("\t", " "))))
+
+# TEST READ IN JAVA AND GET COMMENTS
+with open('PartServiceImpl.txt', 'r') as myfile:
+    rawText = myfile.read()
+getComments(rawText)
+pprint.pprint(comments)
+
+# TEST PORTER STEMMER
 stemmer = PorterStemmer()
 sampleWords = ['ElementTreeSelectionDialog', 'ProgressInfoItem', 'DelegatingStyledCellLabelProvider']
-
 stemmedWords = dict.fromkeys(sampleWords, [])
 
 for word in stemmedWords:
